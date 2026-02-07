f31809fde9dab9492e88c52f7c697c943531079d
Mark Padilla <markjjpadilla@gmail.com>
2026-02-07 02:18:36 +0000

    Convert to an 11ty project so we can load JSON and Markdown files later

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+export default function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("assets");
+
+	return {
+		templateFormats: ["html", "css", "js", "png"],
+		dir: {
+			input: "src"
+		}
+	}
+}
