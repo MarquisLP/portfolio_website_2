796c982466dc6641d4704a6836c151c4ffb018c0
Mark Padilla <markjjpadilla@gmail.com>
2026-02-14 03:29:40 +0000

    Add favicon

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,16 @@ import { DateTime } from 'luxon';
 
 export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('assets');
+	eleventyConfig.addPassthroughCopy({ favicon: '/' });
+	eleventyConfig.addShortcode('faviconHeaderLinks', function () {
+		return `
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+			<link rel="manifest" href="/site.webmanifest">
+		`;
+	});
+
 	eleventyConfig.addPlugin(RenderPlugin);
 
 	eleventyConfig.addFilter('yearsSince', (startDate) => {
